70d10a697d73d0cc0fbbc100a4c62a11b1045926
Jeroen <99068895@Mydavici.nl>
2021-12-16 09:59:19 +0100

    lightswitch klaar

--- lightswitch.py
+++ lightswitch.py
@@ -1,13 +1,25 @@
 import tkinter as tk
 window = tk.Tk()
-
-button = tk.Button(text='...', bg="white", fg="black")
-button.pack(pady = 20, padx = 20)
-
+count = 0
+a = 0
 # schijf hier tussen je code
+def knop(a):
+    global button, count
+    if count == 0:
+        print("licht aan.")
+        button.config(text='Light on.', bg="white", fg='Black')
+        window.config(bg="yellow")
+        count += 1
+    else:
+        print("licht uit.")
+        button.config(text='Light off.', bg='white', fg='black')
+        window.config(bg="black")
+        count -= 1
 
-
-
+button = tk.Button(text='light off.', bg="white", fg="black")
+window.config(bg='black')
+button.pack(pady = 50, padx = 60)
+button.bind('<Button>', knop)
 # schijf hier tussen je code
 
 window.mainloop()
